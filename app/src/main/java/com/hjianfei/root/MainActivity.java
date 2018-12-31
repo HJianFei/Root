c29956532f590e7d55d398dac7df54efaf223c35
HJianFei <190766172@qq.com>
2018-12-31 13:36:56 +0800

    finished

--- app/src/main/java/com/hjianfei/root/MainActivity.java
+++ app/src/main/java/com/hjianfei/root/MainActivity.java
@@ -1,80 +1,161 @@
 package com.hjianfei.root;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.tu.loadingdialog.LoadingDailog;
 import com.hjianfei.service.TrackerService;
 import com.hjianfei.utils.BaseApplication;
 import com.hjianfei.utils.EventBean;
+import com.hjianfei.utils.ToastUtil;
 import com.hjianfei.utils.Utils;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    private LoadingDailog dialog;
+
+    private static String IMEI = "866146032212627";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EventBus.getDefault().register(this);
         setContentView(R.layout.activity_main);
+        initDta();
         findViewById(R.id.tv_is_root).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Utils.isRoot()) {
-                    Toast.makeText(MainActivity.this, "你的手机已获取root权限", Toast.LENGTH_SHORT).show();
+                if (Utils.getIMEI(MainActivity.this).equals(IMEI)) {
+                    if (Utils.isRoot()) {
+                        Toast.makeText(MainActivity.this, "你的手机已获取root权限", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "你的手机没有root权限", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(MainActivity.this, "你的手机没有root权限", Toast.LENGTH_SHORT).show();
+                    ToastUtil.showShortToast("你没有授权使用该软件，请与管理员联系");
                 }
             }
         });
         findViewById(R.id.tv_get_root).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean b = Utils.upgradeRootPermission(getPackageCodePath());
-                if (b) {
-                    Toast.makeText(MainActivity.this, "已获取root权限", Toast.LENGTH_SHORT).show();
+                if (Utils.getIMEI(MainActivity.this).equals(IMEI)) {
+                    boolean b = Utils.upgradeRootPermission(getPackageCodePath());
+                    if (b) {
+                        Toast.makeText(MainActivity.this, "已获取root权限", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "获取root权限失败", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(MainActivity.this, "获取root权限失败", Toast.LENGTH_SHORT).show();
+                    ToastUtil.showShortToast("你没有授权使用该软件，请与管理员联系");
                 }
             }
         });
         findViewById(R.id.tv_get_assemble).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+                if (Utils.getIMEI(MainActivity.this).equals(IMEI)) {
+                    startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+                } else {
+                    ToastUtil.showShortToast("你没有授权使用该软件，请与管理员联系");
+                }
             }
         });
         findViewById(R.id.tv_start_service).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-
-                if (Settings.canDrawOverlays(MainActivity.this)) {
-                    Intent intent = new Intent(MainActivity.this, TrackerService.class);
-                    startService(intent);
-                    showNormalDialog();
+                if (Utils.getIMEI(MainActivity.this).equals(IMEI)) {
+                    if (Settings.canDrawOverlays(MainActivity.this)) {
+                        Intent intent = new Intent(MainActivity.this, TrackerService.class);
+                        startService(intent);
+                        showNormalDialog();
+                    } else {
+                        //若没有权限，提示获取.
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                        Toast.makeText(MainActivity.this, "需要使用悬浮窗权限，监测游戏封号行为", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
+                    }
                 } else {
-                    //若没有权限，提示获取.
-                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-                    Toast.makeText(MainActivity.this, "需要使用悬浮窗权限，监测游戏封号行为", Toast.LENGTH_SHORT).show();
-                    startActivity(intent);
+                    ToastUtil.showShortToast("你没有授权使用该软件，请与管理员联系");
                 }
             }
         });
         findViewById(R.id.tv_end_service).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Utils.removeFile(BaseApplication.tempPath, BaseApplication.gamePath, 2);
+                if (Utils.getIMEI(MainActivity.this).equals(IMEI)) {
+                    Utils.removeFile(BaseApplication.tempPath, BaseApplication.gamePath, 2);
+                } else {
+                    ToastUtil.showShortToast("你没有授权使用该软件，请与管理员联系");
+                }
             }
         });
+    }
+
+    private void initDta() {
+
+
+        //所要申请的权限
+        String[] perms = {Manifest.permission.READ_PHONE_STATE};
+
+        if (EasyPermissions.hasPermissions(this, perms)) {//检查是否获取该权限
+//            Log.i(TAG, "已获取权限");
+            showLoadingDialog();
+
+        } else {
+            //第二个参数是被拒绝后再次申请该权限的解释
+            //第三个参数是请求码
+            //第四个参数是要申请的权限
+            EasyPermissions.requestPermissions(this, "必要的权限", 0, perms);
+        }
+
+    }
+
+    private void showLoadingDialog() {
+        LoadingDailog.Builder loadBuilder = new LoadingDailog.Builder(this)
+                .setMessage("数据校验中...")
+                .setCancelable(false)
+                .setCancelOutside(false);
+        dialog = loadBuilder.create();
+        dialog.show();
+        if (Utils.getIMEI(this).equals(IMEI)) {
+
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    if (dialog != null) {
+                        dialog.dismiss();
+                    }
+                    ToastUtil.showShortToast("数据校验成功");
+                }
+            }, 2000);
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    if (dialog != null) {
+                        dialog.dismiss();
+                    }
+                    ToastUtil.showShortToast("数据校验失败");
+                }
+            }, 2000);
+        }
     }
 
     private void showNormalDialog() {
@@ -126,5 +207,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //把申请权限的回调交由EasyPermissions处理
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    //下面两个方法是实现EasyPermissions的EasyPermissions.PermissionCallbacks接口
+    //分别返回授权成功和失败的权限
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+//        Log.i(TAG, "获取成功的权限" + perms);
+        showLoadingDialog();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+//        Log.i(TAG, "获取失败的权限" + perms);
+        ToastUtil.showShortToast("专用拦截没有获取必要权限");
     }
 }
--- app/src/main/java/com/hjianfei/utils/Utils.java
+++ app/src/main/java/com/hjianfei/utils/Utils.java
@@ -1,5 +1,8 @@
 package com.hjianfei.utils;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
@@ -123,5 +126,30 @@ public class Utils {
 
             }
         }).start();
+    }
+
+    /**
+     * 获取手机IMEI
+     *
+     * @param context
+     * @return
+     */
+    public static final String getIMEI(Context context) {
+        try {
+            //实例化TelephonyManager对象
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            //获取IMEI号
+            @SuppressLint({"HardwareIds", "MissingPermission"})
+            String imei = telephonyManager.getDeviceId();
+            //在次做个验证，也不是什么时候都能获取到的啊
+            if (imei == null) {
+                imei = "";
+            }
+            return imei;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
     }
 }
